1e193aa6320e5fe0beef57f5c2b80af058221046
Evan Zhao <zevany16@gmail.com>
2023-08-03 17:59:40 -0700

    Source Code

---
+++ PhoneAuth/PhoneAuth/Controllers/AccountViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AccountViewController.swift
+//  PhoneAuth
+//
+//  Created by Evan Zhao on 8/3/23.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
